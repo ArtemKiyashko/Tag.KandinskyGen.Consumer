45e778db5a18772ec400c1decd49a8aede925e00
Artem Kiyashko <artem86@hotmail.co.uk>
2024-10-09 12:24:11 +0200

    Make BaseAddress not nullable

--- Tag.KandinskyGen.Managers/KandinskyOptions.cs
+++ Tag.KandinskyGen.Managers/KandinskyOptions.cs
@@ -5,7 +5,7 @@ public class KandinskyOptions
     public uint PictureWidth { get; set; } = 1024;
     public uint PictureHeight { get; set; } = 1024;
     public string? PictureStyle { get; set; }
-    public Uri? BaseAddress { get; set; } = new Uri("https://api-key.fusionbrain.ai");
+    public Uri BaseAddress { get; set; } = new Uri("https://api-key.fusionbrain.ai");
     public string? XKey { get; set; }
     public string? XSecret { get; set; }
 }
